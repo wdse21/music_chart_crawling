57562e8d914607be2d51770a250ba3fbf502c6ba
wdse21 <95352784+wdse21@users.noreply.github.com>
2025-01-17 00:04:13 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# music_chart_crawling
+## 멜론
+- 새로운 노래 50개 조회
+- 멜론 차트 100위 조회
+
+</br>
+
+## 바이브
+- 새로운 앨범 종합 20개 조회
+- 국내 급상승 100위 조회
+- 해외 급상승 100위 조회
+
+</br>
+
+### default
+- **install**
+
+  <pre>
+  <code>
+  python3 -m venv .venv  
+  </code>
+  </pre>
+
+  <pre>
+  <code>
+  poetry config virtualenvs.in-project true
+  </code>
+  </pre>
+
+  <pre>
+  <code>
+  poetry env use python3
+  </code>
+  </pre>
+
+  <pre>
+  <code>
+  poetry install
+  </code>
+  </pre>
+
+  <pre>
+  <code>
+  poetry shell
+  </code>
+  </pre>
+
+- **.env**
+
+  <pre>
+  <code>
+  DOWNLOAD_PATH = "my download path"
+  </code>
+  </pre>
+
+</br>
+
+### run
+- **terminal**
+
+  <pre>
+  <code>
+  cd app
+  </code>
+  </pre>
+
+  <pre>
+  <code>
+  python3 crawling_melon_chart.py
+  </code>
+  </pre>
---
+++ app/melon/crawling_melon_top100_chart.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+import json
+load_dotenv()
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.melon.com/chart/',headers=headers)
+ 
+soup = BeautifulSoup(data.text, 'html.parser')
+
+setTop_50 = soup.select("#lst50")
+setTop_100 = soup.select("#lst100")
+
+rank_array = []
+title_array = []
+artist_array = []
+save_array = []
+
+try:
+    for data in setTop_50 :
+        rank = data.select_one("td:nth-child(2) > div.wrap.t_center > span.rank").text
+        if(rank):
+            rank_array.append(rank)
+
+        title = data.select_one("td:nth-child(6) > div.wrap > div.wrap_song_info > div.ellipsis.rank01 > span > a").text
+        if(title):
+            title_array.append(title)
+
+        artist = data.select_one("td:nth-child(6) > div.wrap > div.wrap_song_info > div.ellipsis.rank02 > a")
+        if(len(artist) > 1):
+            artist_array.append([i.text for i in artist])
+        else :
+            for i in artist:
+                artist_array.append(i.text)
+
+    for data in setTop_100:
+        rank = data.select_one("td:nth-child(2) > div.wrap.t_center > span.rank")
+        if(rank):
+            rank_array.append(rank.text)
+
+        title = data.select_one("td:nth-child(6) > div.wrap > div.wrap_song_info > div.ellipsis.rank01 > span > a")
+        if(title):
+            title_array.append(title.text)
+
+        artist = data.select_one("td:nth-child(6) > div.wrap > div.wrap_song_info > div.ellipsis.rank02 > a")
+        if(len(artist) > 1):
+            artist_array.append([i.text for i in artist])
+        else :
+            for i in artist:
+                artist_array.append(i.text)
+
+    for j, k, n in zip(rank_array, title_array, artist_array):
+        save_array.append({ "rank" : j, "title" : k, "artist" : n })
+
+    file_path = "melon_top100_chart_100.txt"
+    with open(f"{os.environ.get('DOWNLOAD_PATH')}{file_path}", "w", encoding="utf-8") as file:
+        file.write(json.dumps(save_array, indent=4, ensure_ascii=False))
+except Exception as err:
+    print(err)
+    print("에러 발생")
+
+
+        
+
+
+
